4b9ff20dad78de68fa63b092a1b3dc3099c49cba
nguyen dang <nguyenhuudang1617@gmail.com>
2021-03-21 17:58:08 +0700

    ve_12

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
       {/* <StateDemo /> */}
       {/* <BaiTapChonXe /> */}
       {/* <RenderWithMap /> */}
-      <BaiTapthucHanhLayout />
+      {/* <BaiTapthucHanhLayout /> */}
     </div>
 
   );
